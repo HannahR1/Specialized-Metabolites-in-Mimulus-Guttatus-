8a1aa43853b14a0d9f94f9cf1a6776660b51204f
HannahR1 <50858562+HannahR1@users.noreply.github.com>
2019-12-21 17:44:12 -0500

    Update curatePspg.sh

--- curatePspg.sh
+++ curatePspg.sh
@@ -4,6 +4,15 @@
 #For annotations that have a full PSPG box,  writes the IDs to a new file. 
 python pspgBox.py
 
+#take out non-primary sequences 
+#NOTE: change the input and output: 
+#infile2 = 
+#outfile2= 
+python parsePrimary2.py
+
+#check that this is a subset of the initial HMMER hits 
+checkSubset.py
+
 #Get full sequences that have a full PSPG box
 #NOTE: change the input and output: 
 #infile2 = "PspgID.txt"
