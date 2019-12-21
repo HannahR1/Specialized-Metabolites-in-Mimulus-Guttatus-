8fe1c653534b0e4c48046ec47892851b253b4bdb
HannahR1 <50858562+HannahR1@users.noreply.github.com>
2019-12-20 19:02:13 -0500

    Update HMMsearch.sh

--- HMMsearch.sh
+++ HMMsearch.sh
@@ -1,17 +1,16 @@
 
+#!/bin/bash --login
+########## SBATCH Lines for Resource Request ##########
 
-
-
-
-
-
-
-
-
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu= 2G
+#SBATCH --job-name HMMERsearch
 
 #run this command to start using HMMER methods 
 export PATH=$PATH:/$HOME/Documents/HMMER/hmmer-3.2.1/hmmer-3.2.1/src/
-
 
 #hmmsearch 
 hmmsearch -A MimUGTs.sto UDPGT.hmm MguttatusSeq.fasta > hmmsearch.out 
