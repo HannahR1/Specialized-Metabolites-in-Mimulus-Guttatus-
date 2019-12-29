f1d8304ac8d94e423888c84c711319188cf722d2
HannahR1 <50858562+HannahR1@users.noreply.github.com>
2019-12-28 19:11:20 -0500

    Update curatePspg.sh

--- curatePspg.sh
+++ curatePspg.sh
@@ -13,12 +13,10 @@ python parsePrimary2.py
 #check that this is a subset of the initial HMMER hits 
 checkSubset.py
 
-#remove fullseqs??
+python RemoveWhiteSpc2.py
 
-#Get full sequences that have a full PSPG box
-#NOTE: change the input and output: 
-#infile2 = "PspgID.txt"
-#outfile2= "Mim.UGT.v3"
+python Format2.py
+
 python GetFullSeqs.py
 
 
