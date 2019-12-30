eafc00626a1dbfe9f2ff3fa6663375f06b222093
HannahR1 <50858562+HannahR1@users.noreply.github.com>
2019-12-30 11:26:02 -0500

    Update curateSize.sh

--- curateSize.sh
+++ curateSize.sh
@@ -3,7 +3,6 @@
 
 
 
-
 #Removes the white space from the sequences Input: Mim.UGT.v2.fasta Output: Mim.UGT.v2.Strp.fasta
 python strpSeqs.py
 
@@ -13,3 +12,9 @@ python Format2.py
 
 #For annotations that fall within this threshold between 200 and 550 amino acids long, it writes the sequence to a new file.
 python GetSize.py newfile name -->  GetFullSeqSize.py 
+
+###new commands###
+python RemoveWhiteSpc3.py
+python Format3.py
+python GetFullSeqSize.py
+
