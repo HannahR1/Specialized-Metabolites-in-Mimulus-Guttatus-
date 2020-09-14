74fbb8fd525c249dfef134c656ae85c3ac186fe3
HannahR1 <50858562+HannahR1@users.noreply.github.com>
2020-09-14 17:31:28 -0400

    Create tree3.sh

---
+++ tree3.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash --login
+########## SBATCH Lines for Resource Request ##########
+
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu= 2G
+#SBATCH --job-name HMMERsearch
+
+
+#align mimulus pfam sequences and the markers
+#sequences need to be the same length for FASTTREE
+$HOME/Documents/MUSCLE/./muscle -in tree3.fasta -out tree3.out
+
+
+#make nexus file with tree information
+$HOME/Documents/FASTREE/www.microbesonline.org/fasttree/./FastTree tree3.out > tree3.nex
